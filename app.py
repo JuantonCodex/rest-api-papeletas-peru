f15b9ada326747e92b376ade0b8fea4808f222f1
Juan Antonio <jcahuanam@gmail.com>
2022-04-16 19:52:30 -0500

    Update gitignore

--- app.py
+++ app.py
@@ -10,4 +10,5 @@ def papeletas(_dni):
     papeletas = getPapeletas(_dni)
     return papeletas
 
-app.run(debug=False, port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
