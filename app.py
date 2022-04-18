8336deecefa58933c3222b09de54c3903f19f9aa
Juan Antonio <jcahuanam@gmail.com>
2022-04-18 17:16:08 -0500

    Add cors

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import os
 from flask import Flask
+from flask_cors import CORS, cross_origin
 from functions import getPapeletas
 
 
 app = Flask(__name__)
-# print(os.environ['ANTICAPTCHA_TOKEN'])
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/papeletas/<string:_dni>", methods=["GET"])
+@cross_origin()
 def papeletas(_dni):
     papeletas = getPapeletas(_dni)
     return papeletas
